8a06de804b4917ca5e572fcff8f07db0ec2c74e3
Vitaliy Novichenko <novichenkovitaliy@gmail.com>
2025-07-31 00:49:02 +0300

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Vitaliy Novichenko on 18.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
